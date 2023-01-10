29f7ed82c4f6570ae082f0c583c7bface6d1aa41
hamzabelakri <hamzabelakri92@gmail.com>
2023-01-10 15:03:52 +0100

    fix upload image

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const connect=require('./config/connectDb')
 require('dotenv').config({path:"./config/.env"})
+const bodyParser = require('body-parser');
+
 const cors=require('cors')
 const postRouter=require('./routes/postRouter')
 
 const app=express()
 
-app.use(express.json())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 app.use(cors());
 
